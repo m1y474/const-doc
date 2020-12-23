a20eba3206036245c959e3376d9141d5b5f93d07
rina.miyata <rina.miyata@eilsystem.com>
2020-12-23 11:26:12 +0900

    fix: シングルクォートの追加

--- convert.php
+++ convert.php
@@ -4,5 +4,5 @@ require_once('./enum.php');
 $ref = new ReflectionClass(Enum::class);
 foreach ($ref->getConstants() as $key => $value) {
     $const = new ReflectionClassConstant($ref->getName(), $key);
-    echo $ref->getName().'::' . $const->name . ' => ' . str_replace(' */', '', (str_replace('/** @var int ', '', $const->getDocComment()))) . ",\n";
+    echo $ref->getName().'::' . $const->name . " => '" . str_replace(' */', '', (str_replace('/** @var int ', '', $const->getDocComment()))) . "',\n";
 }
